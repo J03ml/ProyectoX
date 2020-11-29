65d5d253b45a7106382e6b607799c3147eac7ab1
unknown <kikiss2002@hotmail.com>
2020-11-29 21:09:48 +0100

    second commit

---
+++ PHP/index.php
@@ -0,0 +1,14 @@
+<?php include("db.php") ?>
+
+<!DOCTYPE html>
+<html lang="en"> 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="whit-device">
+    <meta http-equip="X--UA-Compatible"content="ie-edge">
+    <title>Proyecto estudiantil</title>
+</head>
+<body>
+
+</body>
+</html>
