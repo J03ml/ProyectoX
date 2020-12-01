06aded1bd3ea43463b3a8e495f9aa3a2e1fdce63
AliagaRevilla <75218794+AliagaRevilla@users.noreply.github.com>
2020-12-01 02:37:37 +0100

    Update index.php

--- PHP/index.php
+++ PHP/index.php
@@ -1,14 +1,29 @@
-<?php include("db.php") ?>
+<?php  include ("db.php")?>
 
-<!DOCTYPE html>
-<html lang="en"> 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="whit-device">
-    <meta http-equip="X--UA-Compatible"content="ie-edge">
-    <title>Proyecto estudiantil</title>
-</head>
-<body>
+<?php include("includes/header.php") ?>
 
-</body>
-</html>
+<div class="container p-4">
+    
+    <div class="row">
+
+        <div class="col-md-10">
+
+            <div class="card card-body">
+                <form action="save_task.php" method="POST">
+                    <div class="form-group">
+                        <input type="text" name="title" class="form control"
+                        placeholder="Username" autofocus>
+                    </div>  
+                    <div class="form-group">    
+                        <textarea name="description" rows="2" class="form-control"
+                        placeholder="Commentary"></textarea>
+                    </div>
+                    <input type="submit" class="btn btn-danger btn-block"
+                    name="saved_task" value="Comment">     
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include("includes/footer.php") ?>
